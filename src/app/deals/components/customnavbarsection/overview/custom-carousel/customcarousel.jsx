df6796d6e5c00d07f16643239858188cc31d81ba
akash101997 <akash.singh@webninjaz.com>
2025-09-10 16:36:55 +0530

    Fixed the text glow of menu section

--- src/app/deals/components/customnavbarsection/overview/custom-carousel/customcarousel.jsx
+++ src/app/deals/components/customnavbarsection/overview/custom-carousel/customcarousel.jsx
@@ -1,20 +1,27 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./customcarousel.css";
-
 import { Carousel } from "react-bootstrap";
 
 const FirstCarousel = () => {
-  const [key, setKey] = useState("Overview");
+  const [index, setIndex] = useState(0);
 
-  const images = [
-    "/assets/pictures/video.png",
-    "/assets/pictures/video.png",
-    "/assets/pictures/video.png",
-    "/assets/pictures/video.png",
+  // Mixed list: images + YouTube video
+  const media = [
+    "https://www.youtube.com/watch?v=7wtfhZwyrcc&list=RD7wtfhZwyrcc&start_radio=1",
+    "https://www.youtube.com/watch?v=7wtfhZwyrcc&list=RD7wtfhZwyrcc&start_radio=1",
+    "https://www.youtube.com/watch?v=7wtfhZwyrcc&list=RD7wtfhZwyrcc&start_radio=1", // example YouTube link
+    "https://www.youtube.com/watch?v=7wtfhZwyrcc&list=RD7wtfhZwyrcc&start_radio=1",
   ];
 
-  const [index, setIndex] = useState(0);
+  // helper: convert youtube link → embed format
+  const getYoutubeEmbedUrl = (url) => {
+    const regExp =
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+    const match = url.match(regExp);
+    return match && match[2].length === 11
+      ? `https://www.youtube.com/embed/${match[2]}?rel=0&modestbranding=1`
+      : null;
+  };
 
   return (
     <div className="customcars">
@@ -23,18 +30,35 @@ const FirstCarousel = () => {
         onSelect={() => {}}
         controls={false}
         indicators={false}
-        interval={null} // disable default auto-slide since we handle it manually
+        interval={null}
       >
-        {images.map((img, i) => (
-          <Carousel.Item key={i}>
-            <img
-              className="d-block w-100"
-              src={img}
-              alt={`Slide ${i + 1}`}
-              style={{ height: "fit-content", objectFit: "contain" }}
-            />
-          </Carousel.Item>
-        ))}
+        {media.map((item, i) => {
+          const isYoutube = item.includes("youtube.com") || item.includes("youtu.be");
+          return (
+            <Carousel.Item key={i}>
+              {isYoutube ? (
+                <div className="video-wrapper">
+                  <iframe
+                    width="100%"
+                    height="400"
+                    src={getYoutubeEmbedUrl(item)}
+                    title={`YouTube video ${i + 1}`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              ) : (
+                <img
+                  className="d-block w-100"
+                  src={item}
+                  alt={`Slide ${i + 1}`}
+                  style={{ height: "fit-content", objectFit: "contain" }}
+                />
+              )}
+            </Carousel.Item>
+          );
+        })}
       </Carousel>
 
       <div className="d-flex mt-3 buttons justify-content-center align-items-center lastbtns">
@@ -43,7 +67,7 @@ const FirstCarousel = () => {
           className="custom-carousel-prev"
           onClick={() =>
             setIndex((prevIndex) =>
-              prevIndex === 0 ? images.length - 1 : prevIndex - 1
+              prevIndex === 0 ? media.length - 1 : prevIndex - 1
             )
           }
         >
@@ -66,7 +90,7 @@ const FirstCarousel = () => {
 
         {/* Indicators */}
         <div className="custom-carousel-indicators mx-3 d-flex justify-content-center">
-          {images.map((_, i) => (
+          {media.map((_, i) => (
             <span
               key={i}
               className={`indicator-shape ${index === i ? "active" : ""}`}
@@ -78,9 +102,7 @@ const FirstCarousel = () => {
         {/* Next */}
         <button
           className="custom-carousel-next"
-          onClick={() =>
-            setIndex((prevIndex) => (prevIndex + 1) % images.length)
-          }
+          onClick={() => setIndex((prevIndex) => (prevIndex + 1) % media.length)}
         >
           <svg
             width="8"
