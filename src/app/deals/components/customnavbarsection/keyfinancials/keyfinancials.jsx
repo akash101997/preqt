43096d781eda9825d90281d562f9a7b304d9c415
rahul-verma-30 <rahul.verma@webninjaz.com>
2025-09-16 09:14:26 +0530

    build erro fixed

--- src/app/deals/components/customnavbarsection/keyfinancials/keyfinancials.jsx
+++ src/app/deals/components/customnavbarsection/keyfinancials/keyfinancials.jsx
@@ -1,4 +1,4 @@
-  "use client";
+"use client";
 import React, { useState } from "react";
 import Barchart from "../charts/barchart/barchart";
 import PurpleBarchart from "../charts/barchartpurple/barchartpurple";
@@ -12,7 +12,7 @@ import { useSearchParams } from "next/navigation";
 const Keyfinancials = () => {
   const searchParams = useSearchParams();
   const dealId = searchParams?.get("dealId");
-
+  const isPrivateDeal = dealId == "2";
 
   const [key, setKey] = useState("Return on Equity (ROE)");
 
@@ -57,10 +57,9 @@ const Keyfinancials = () => {
       [year]: !prev[year],
     }));
   };
- console.log("guvjkhygfcfg", isPrivateDeal);
   return (
     // <div className="key-financials-container">
-    <div  className={`key-financials-container ${isPrivateDeal ? "private-deal" : ""}`}>
+    <div className={`key-financials-container ${isPrivateDeal ? "private-deal" : ""}`}>
 
 
       {/* Financial Trends */}
