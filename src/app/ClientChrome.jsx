1604089e895a039efcd93568ce2fa8bfdfa8bc24
anukaran-maurya1435 <maurya.anukaran1@gmail.com>
2025-09-26 11:24:05 +0530

    fixed boder radius od ustilisation fund

--- src/app/ClientChrome.jsx
+++ src/app/ClientChrome.jsx
@@ -98,6 +98,7 @@ export default function ClientChrome({ children }) {
     return <>{children}</>;
   }
 
+  // console.log("ddssfsfvfe",isPrivateDeal)
   return (
     <div
       className={isPrivateDeal ? "private-deal-theme" : ""}
@@ -111,6 +112,24 @@ export default function ClientChrome({ children }) {
       <div style={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
         {children}
       </div>
+
+
+    <div className="newNav"   style={{
+    marginBottom: isPrivateDeal ? "110px" : "0px"
+  }}
+>
+
+
+    <div className="newNav"   style={{
+    marginBottom: isPrivateDeal ? "110px" : "0px"
+  }}
+>
+      <NewFooter/>
+  
+
+</div>
+    
+        {/* <Footer /> */}
 
       <div
         className="newNav"
