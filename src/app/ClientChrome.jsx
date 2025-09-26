d00e40de0328635f1239220afe9325dd32417f99
anukaran-maurya1435 <maurya.anukaran1@gmail.com>
2025-09-26 11:24:43 +0530

    fix github

--- src/app/ClientChrome.jsx
+++ src/app/ClientChrome.jsx
@@ -98,7 +98,6 @@ export default function ClientChrome({ children }) {
     return <>{children}</>;
   }
 
-  // console.log("ddssfsfvfe",isPrivateDeal)
   return (
     <div
       className={isPrivateDeal ? "private-deal-theme" : ""}
@@ -114,24 +113,7 @@ export default function ClientChrome({ children }) {
         {children}
       </div>
 
-
-    <div className="newNav"   style={{
-    marginBottom: isPrivateDeal ? "110px" : "0px"
-  }}
->
-
-
-    <div className="newNav"   style={{
-    marginBottom: isPrivateDeal ? "110px" : "0px"
-  }}
->
-      <NewFooter/>
-  
-
-</div>
-    
-        {/* <Footer /> */}
-
+      {/* Footer container */}
       <div
         className="newNav"
         style={{
